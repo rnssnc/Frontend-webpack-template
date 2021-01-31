5e15fafd107147aac776bbc62cf40f8da8f272ed
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-01-31 23:04:26 +0300

    fix: add publicPath for fonts file-loader

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -167,6 +167,7 @@ module.exports = {
         test: /\.(ttf|woff|woff2|eot)$/,
         loader: 'file-loader',
         options: {
+          publicPath: './',
           name: 'fonts/[name].[ext]',
         },
       },
